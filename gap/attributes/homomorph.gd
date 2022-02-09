c8dd4a367e59d5bb92ea2a520cd33b072e37c43a
Artemis Konstantinidi <ak316@st-andrews.ac.uk>
2022-02-09 15:55:37 +0000

    Add homomorph.gi and homomorph.gd

---
+++ gap/attributes/homomorph.gd
@@ -0,0 +1,11 @@
+#############################################################################
+##
+##  homomorph.gd
+##  Copyright (C) 2022                                Artemis Konstantinidi
+##                                                    Chinmaya Nagpal
+##  Licensing information can be found in the README file of this package.
+##
+#############################################################################
+##
+
+DeclareOperation("SemigroupHomomorphismByImages", [IsSemigroup, IsSemigroup, IsList, IsList]);
